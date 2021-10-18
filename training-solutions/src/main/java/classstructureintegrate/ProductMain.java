d06ffff70742af5a4c9d69f397203f363a29059e
karikasg <karikas.gabor@gmail.com>
2021-10-18 15:31:28 +0200

    UML, példányok konzol íráskor/olvasáskor 1. feladat

---
+++ training-solutions/src/main/java/classstructureintegrate/ProductMain.java
@@ -0,0 +1,25 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class ProductMain {
+    public static void main(String[] args) {
+        String name;
+        int price;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kérem a termék megnevezését: ");
+        name = scanner.nextLine();
+        System.out.print("Kérem a termék kiinduló árát: ");
+        price = scanner.nextInt();scanner.nextLine();
+        Product product = new Product(name, price);
+        System.out.println("Az alábbi terméket regisztráltuk:  név: " + product.getName() + ", ára: " + product.getPrice());
+        System.out.print("\nMennyivel növeljük az árat? ");
+        product.increasePrice(scanner.nextInt());scanner.nextLine();
+        System.out.println("A termék új adatai:  név: " + product.getName() + ", ára: " + product.getPrice());
+        System.out.print("\nMennyivel csökkentsük az árat? ");
+        product.decreasePrice(scanner.nextInt());scanner.nextLine();
+        System.out.println("A termék új adatai:  név: " + product.getName() + ", ára: " + product.getPrice());
+
+    }
+}
