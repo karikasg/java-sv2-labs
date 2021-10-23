e75d142a0eff88dc5a4c936d370e3e17289e9f36
Kari <karikas.gabor@gmail.com>
2021-10-23 14:39:07 +0200

    JAVA Api 1. feladat

---
+++ training-solutions/src/main/java/javaapi/UpperMain.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class UpperMain {
+    public static void main(String[] args) {
+        Upper up = new Upper();
+        String text = "Hello World";
+        System.out.println(up.upper(text));
+    }
+}
