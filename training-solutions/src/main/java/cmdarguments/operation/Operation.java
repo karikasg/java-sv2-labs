0a6e4c60a52bd2495d090e81a11cf06e9bd678f0
karikasg <karikas.gabor@gmail.com>
2021-10-26 12:25:57 +0200

    Parancssori paraméterek 3. feladat (próba más megoldás)

--- training-solutions/src/main/java/cmdarguments/operation/Operation.java
+++ training-solutions/src/main/java/cmdarguments/operation/Operation.java
@@ -3,15 +3,11 @@ package cmdarguments.operation;
 public class Operation {
     public static void main(String[] args) {
         for (String arg : args){
-            switch (arg){
-                case "/list":
-                    System.out.println("Listázás");break;
-                case "/add":
-                    System.out.println("Hozzáadás");break;
-                case "/delete":
-                    System.out.println("Törlés");break;
-                default:
-                    System.out.println("Ismeretlen művelet");
+            switch (arg) {
+                case "/list" -> System.out.println("Listázás");
+                case "/add" -> System.out.println("Hozzáadás");
+                case "/delete" -> System.out.println("Törlés");
+                default -> System.out.println("Ismeretlen művelet");
             }
         }
 
