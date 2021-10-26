fae72a90f4c55cb41d5f6511d3f3036f23c974bc
karikasg <karikas.gabor@gmail.com>
2021-10-26 12:03:47 +0200

    Parancssori paraméterek 2. feladat (csere for-each-re)

--- training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
+++ training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
@@ -2,8 +2,8 @@ package cmdarguments.verse;
 
 public class VerseMain {
     public static void main(String[] args) {
-        for (int i=0; i<args.length; i++){
-            System.out.print(args[i]+" ");
+        for (String arg : args) {
+            System.out.print(arg + " ");
         }
         System.out.println();
 
