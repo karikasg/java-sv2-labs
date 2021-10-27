2cb53bce594ae27c5a705afef4eab597b526ebd7
karikasg <karikas.gabor@gmail.com>
2021-10-27 22:07:11 +0200

    Bevezetés az attribútumok használatába 1. feladat

---
+++ training-solutions/src/main/java/attributes/book/BookMain.java
@@ -0,0 +1,10 @@
+package attributes.book;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("Egri csillagok");
+        System.out.println(book.getTitle());
+        book.setTitle("Aranyember");
+        System.out.println(book.getTitle());
+    }
+}
