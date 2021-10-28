9c85ca38789eb3e133d5f01eb5c7021991bb54d8
karikasg <karikas.gabor@gmail.com>
2021-10-28 17:28:45 +0200

    Bevezetés a metódusok használatába 1. feladat (.toString nem kell)

--- training-solutions/src/main/java/intromethods/employee/EmployeeMain.java
+++ training-solutions/src/main/java/intromethods/employee/EmployeeMain.java
@@ -4,8 +4,8 @@ public class EmployeeMain {
     public static void main(String[] args) {
         Employee employee = new Employee("János", 2010, 350000);
 
-        System.out.println(employee.toString());
+        System.out.println(employee);
         employee.raiseSalary(40000);
-        System.out.println(employee.toString());
+        System.out.println(employee);
     }
 }
